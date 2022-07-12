c73eb866d570dacdd39e85618faf488fb51f8e2e
Matheus Broseghini <mateus_guarnier@hotmail.com>
2022-07-12 19:46:34 -0300

    Atividade incompleta

---
+++ script.js
@@ -0,0 +1,29 @@
+let produtosCarrinho = document.querySelector(".produtos")
+
+let products = []
+
+function somaDosProdutos(lista){
+   
+    for(let i = 0; i < lista.length; i++){
+   
+        i + i
+   
+    }
+}
+somaDosProdutos()
+
+    let btnComprar  = document.createElement("button")
+
+    if(id != undefined){
+
+        btnComprar.id =  id
+    }
+
+    
+    btnComprar.innerText = "Finalizar Compras"
+
+
+body.appendChild(main)
+main.appendChild(productsList)
+main.appendChild(productsDetails)
+
